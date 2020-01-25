af61ea5e9a52b0cc6fe2b656070b73bd8c3b34d3
Nicolayka <nicolay.atanov@gmail.com>
2020-01-25 21:59:46 +0300

    Максимум из двух чисел[#224724] FIX

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int first, int second) {
-        boolean condition = false;
+        boolean condition = first > second;
         int result = condition ? first : second;
         //int result = right;
         return result;
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,8 +6,18 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMax1() {
+        int result = Max.max(3, 1);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenMax2() {
         int result = Max.max(1, 3);
+        assertThat(result, is(1));
+    }
+    @Test
+    public void whenMaxequaly() {
+        int result = Max.max(3, 3);
         assertThat(result, is(3));
     }
 }
